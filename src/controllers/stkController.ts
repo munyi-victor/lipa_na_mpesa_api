48be7c31678be0d083e5c45f51325e66ecd9decd
Victor Munyi <munyivictor6@gmail.com>
2024-12-09 16:05:01 +0300

    first commit

---
+++ src/controllers/stkController.ts
@@ -0,0 +1,53 @@
+import axios from "axios";
+import { Response } from "express";
+import { timestamp } from "../../utils/timeStamp";
+import { RequestExtended } from "../middlewares/generateToken";
+
+const handleStkPush = async (req: RequestExtended, res: Response) => {
+  const { amount, phone } = req.body;
+
+  const BUSINESS_SHORT_CODE = process.env.MPESA_BUSINESS_SHORT_CODE as string;
+
+  const password = Buffer.from(
+    BUSINESS_SHORT_CODE + process.env.MPESA_PASS_KEY + timestamp
+  ).toString("base64");
+
+  const payload = {
+    BusinessShortCode: BUSINESS_SHORT_CODE,
+    Password: password,
+    Timestamp: timestamp,
+    TransactionType: "CustomerPayBillOnline",
+    Amount: amount,
+    PartyA: phone,
+    PartyB: process.env.MPESA_BUSINESS_SHORT_CODE,
+    PhoneNumber: phone,
+    CallBackURL: "https://mydomain.com/path",
+    AccountReference: "BuySasa",
+    TransactionDesc: "Payment",
+  };
+
+  try {
+    const response = await axios.post(
+      "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest",
+      payload,
+      {
+        headers: {
+          "Content-Type":"application/json",
+          Authorization: `Bearer ${req.token}`,
+        },
+      }
+    );
+    res.status(201).json({
+      message: true,
+      data: response.data,
+    });
+    
+  } catch (error: any) {
+    console.log("phone",phone)
+    res.status(500).json({
+      message: "Failed",
+      error: error,
+    });
+  }
+};
+export { handleStkPush };
